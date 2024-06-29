a40d3b008ba36043439239986a478fbf91e72352
black <1169794338@qq.com>
2024-06-29 20:07:57 +0800

    Flux 关闭

--- src/main/java/com/bootx/controller/TextAppController.java
+++ src/main/java/com/bootx/controller/TextAppController.java
@@ -1,6 +1,9 @@
 package com.bootx.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.bootx.common.Result;
+import com.bootx.util.JsonUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author black
@@ -19,10 +23,19 @@ public class TextAppController extends BaseController {
 
     @PostMapping("/category")
     private Result category(){
-        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select id,name from textappcategory");
-        for (Map<String, Object> map : maps) {
-            map.put("list",jdbcTemplate.queryForList("select id,name,memo from textapp where textAppCategory_id=?",map.get("id")));
+        String s = redisService.get("category");
+        List<Map<String, Object>> maps = new ArrayList<>();
+        try {
+            maps = JsonUtils.toObject(s, new TypeReference<List<Map<String, Object>>>() {
+            });
+        }catch (Exception e){
+            maps = jdbcTemplate.queryForList("select id,name from textappcategory");
+            for (Map<String, Object> map : maps) {
+                map.put("list",jdbcTemplate.queryForList("select id,name,memo from textapp where textAppCategory_id=?",map.get("id")));
+            }
+            redisService.set("category",JsonUtils.toJson(maps),30, TimeUnit.MINUTES);
         }
+
         return Result.success(maps);
     }
 }
