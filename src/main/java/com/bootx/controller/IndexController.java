3a436b015b600c7ec8d7684a538dfe5204b3c92b
blackboy1987 <1111@qq.com>
2024-07-15 07:57:43 +0800

    init

---
+++ src/main/java/com/bootx/controller/IndexController.java
@@ -0,0 +1,30 @@
+package com.bootx.controller;
+
+import com.bootx.common.Result;
+import com.bootx.entity.Member;
+import com.bootx.security.CurrentUser;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author black
+ */
+@RestController("apiIndexController")
+@RequestMapping("/api")
+public class IndexController extends BaseController {
+
+    @PostMapping("/check")
+    private Result check(@CurrentUser Member member, HttpServletRequest request,String version){
+        return Result.success();
+    }
+
+
+
+}
