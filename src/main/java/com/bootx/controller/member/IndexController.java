fdb00a683f0d270cc7bb9fc75bc44cc0c14b0570
blackboy1987 <1111@qq.com>
2024-07-16 07:51:31 +0800

    init

--- src/main/java/com/bootx/controller/member/IndexController.java
+++ src/main/java/com/bootx/controller/member/IndexController.java
@@ -4,9 +4,16 @@ import com.bootx.controller.BaseController;
 import com.bootx.service.ImageTaskService;
 import com.bootx.service.MemberService;
 import com.bootx.service.SmsLogService;
+import com.bootx.util.AiUtils;
+import com.bootx.util.MessagePojo;
 import jakarta.annotation.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Objects;
 
 /**
  * @author black
@@ -24,6 +31,10 @@ public class IndexController extends BaseController {
     @Resource
     private SmsLogService smsLogService;
 
+    @GetMapping(value = "/message",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<MessagePojo> message(String content){
+        return Flux.from(Objects.requireNonNull(AiUtils.message1(content)));
+    }
 
 
 }
